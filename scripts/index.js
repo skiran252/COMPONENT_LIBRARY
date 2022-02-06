3a01bee200635e96b0c2ba6937a76560d71430cd
SAIKIRAN GONUGUNTA <skiran252@gmail.com>
2022-02-06 17:16:39 +0530

    removed target from elements. (reduced code) and modified css accordingly for navigation-items

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,23 @@
-// on complete document load js 
+// on complete document load js
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelectorAll("pre code").forEach((el) => {
+    el.innerHTML = el.innerHTML
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+    hljs.highlightElement(el);
+  });
+});
 
-const navbarItems = document.querySelectorAll(".docs-navbar-item");
+const navbarItems = document.querySelectorAll(".navbar-link");
 navbarItems[0].classList.add("active");
 let prevTarget = navbarItems[0];
-
-document.addEventListener("DOMContentLoaded", (event) => {
-    document.querySelectorAll("pre code").forEach((el) => {
-      el.innerHTML = el.innerHTML
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
-      hljs.highlightElement(el);
-    });
-  });
-  
 document.addEventListener("click", function (event) {
-  if (
-    event.target.classList.contains("docs-navbar-item") 
-    // || event.target.parentNode.classList.contains("docs-navbar-item")
-  ) {
-    
-    let target = event.target;
-    let targetId = target.getAttribute("target");
-    let targetElement = document.getElementById(targetId);
-    console.log(targetElement);
-    //scroll to target
-    targetElement.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-    });
-
-    
-    if (prevTarget) {
-      prevTarget.classList.remove("active");
-    }
+  if (event.target.classList.contains("navbar-link")) {
+    if (prevTarget) prevTarget.classList.remove("active");
     event.target.classList.add("active");
     prevTarget = event.target;
   }
 });
-
